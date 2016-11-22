524fcae75282cd7872e376dbb4cae4f76432fa36
Jason McTigue <jasonmctigue@live.ie>
2016-11-22 23:01:04 +0000

    added variables and keys for satabase

--- SavingsAccumulator/Model/Target.cs
+++ SavingsAccumulator/Model/Target.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SavingsAccumulator.Model
 {
+    [Table("Target")] //Name of the table in the database
     public class Target
     {
+        [Key]//sets Id as the primary key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }// Primary key and  identity column so it will auto incement by itself
         public string Name { get; set; }
-        public int SavingTarget { get; set; }
+        public decimal SavingTarget { get; set; } //is a decimal to allow user to in put cents.
+        public decimal CurrentBalance { get; set; }//keeps track of the current balance
 
         public string Notes { get; set; }
+
+        public DateTime Date { get; set; }
     }
 }
