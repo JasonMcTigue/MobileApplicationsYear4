e284b64b6be6fd92a18e76689433709be1d09223
Jason McTigue <jasonmctigue@live.ie>
2016-11-25 18:13:00 +0000

    added data context helper for database

---
+++ SavingsAccumulator/DataContext/DataContextHelper.cs
@@ -0,0 +1,34 @@
+﻿using SavingsAccumulator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingsAccumulator.DataContext
+{
+    //static class to refer to for adding stuff.
+    public static class DataContextHelper
+    {
+        public async void AddTarget(Target newTarget) {
+
+            using (var db = new TargetDataContext()) {
+                db.Targets.Add(newTarget); //adds a new target to the database
+                await db.SaveChangesAsync();
+            }
+        }
+
+        public async void AddTransation(Target newTransaction)
+        {
+
+            using (var db = new TargetDataContext())
+            {
+                db.Targets.Add(newTransaction); //adds a new target to the database
+                await db.SaveChangesAsync();
+            }
+        }
+
+       
+
+    }
+}
