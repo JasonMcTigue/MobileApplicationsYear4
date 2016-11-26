ee2a158f75fa5b21d89b5b157564b95672e9843e
Jason McTigue <jasonmctigue@live.ie>
2016-11-26 21:33:14 +0000

    coded functionality for transaction control page

--- SavingsAccumulator/UserControls/AddTransactionControl.xaml.cs
+++ SavingsAccumulator/UserControls/AddTransactionControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SavingsAccumulator.DataContext;
+using SavingsAccumulator.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,9 +21,35 @@ namespace SavingsAccumulator.UserControls
 {
     public sealed partial class AddTransactionControl : UserControl
     {
+        public int _targetId;
         public AddTransactionControl()
         {
             this.InitializeComponent();
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearTxtBox();
+        }
+
+        private void ConfirmBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Updates all the fields
+            var newTransaction = new Transaction();
+            newTransaction.Date = DateTime.Now;
+            newTransaction.Amount = Convert.ToDecimal(AmtTxtBox);
+            newTransaction.TargetId = _targetId;
+            DataContextHelper.AddRecord<Transaction>(newTransaction);
+
+            ClearTxtBox();
+        }
+
+        private void SetTargetId(int id) {
+            _targetId = id; //sets the target id so the program knows that the id is associased with that transaction
+        }
+
+        private void ClearTxtBox() {
+            AmtTxtBox.Text = string.Empty;//clears all text from the text box
         }
     }
 }
