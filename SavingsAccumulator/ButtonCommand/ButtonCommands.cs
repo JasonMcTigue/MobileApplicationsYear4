37aca5c2ac4b55c6e31df4ccc695a66deed75dbe
Jason McTigue <jasonmctigue@live.ie>
2016-11-26 22:49:36 +0000

    added button command class

---
+++ SavingsAccumulator/ButtonCommand/ButtonCommands.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SavingsAccumulator.ButtonCommand
+{
+    public class ButtonCommands : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        Action _action;
+
+        public ButtonCommands(Action action) {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
